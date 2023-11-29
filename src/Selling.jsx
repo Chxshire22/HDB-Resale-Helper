b9d8662a7463d4b971ca4eefc799dbcfc9fe0a83
Chxshire22 <akuindra22@gmail.com>
2023-11-29 13:32:42 +0800

    input fields on selling done. left with function

--- src/Selling.jsx
+++ src/Selling.jsx
@@ -10,6 +10,13 @@ export default function Selling(props) {
   const [cpfUsedValue, setCpfUsedValue] = useState("")
   const [accruedValue, setAccruedValue] = useState("")
   const [levyValue, setLevyValue] = useState("")
+  const [nextHdbValue, setNextHdbValue] = useState(false)
+
+  //input is only needed if seller is buying next HDB
+  const [nextHleValue, setNextHleValue] = useState(false)
+  const [disabledNextHleInput, setDisabledNextHleInput] = useState(true)
+
+  //lift this to App.jsx
   let setSalesProceeds = props.setSalesProceeds;
 
   
@@ -28,13 +35,21 @@ export default function Selling(props) {
   let handleLevyOnChange = (e) => {
     setLevyValue(e.target.event)
   };
+  let handleNextHdb = (e) => {
+    setDisabledNextHleInput(disabledNextHleInput? false : true)
+    setNextHdbValue(e.target.checked)
+  };
+  let handleNextHle = (e) => {
+    setNextHleValue(e.target.checked)
+  };
 
 
   //what happens on submit
   
   let handleSubmit = (e) => {
     e.preventDefault();
-    console.log("sellingPriceValue", sellingPriceValue)
+    console.log("sellingPriceValue ", sellingPriceValue)
+    console.log("nextHdbValue ", nextHdbValue)
   };
   return (
     <Form onSubmit={handleSubmit}>
@@ -54,7 +69,11 @@ export default function Selling(props) {
         <Form.Label>Outstanding Mortgage </Form.Label>
         <InputGroup>
           <InputGroup.Text>$</InputGroup.Text>
-          <Form.Control type="number" placeholder="" onChange={handleOutstandingOnChange}/>
+          <Form.Control
+            type="number"
+            placeholder=""
+            onChange={handleOutstandingOnChange}
+          />
         </InputGroup>
       </Form.Group>
 
@@ -62,7 +81,11 @@ export default function Selling(props) {
         <Form.Label>CPF Used </Form.Label>
         <InputGroup>
           <InputGroup.Text>$</InputGroup.Text>
-          <Form.Control type="number" placeholder="" onChange={handleCpfUsedOnChange}/>
+          <Form.Control
+            type="number"
+            placeholder=""
+            onChange={handleCpfUsedOnChange}
+          />
         </InputGroup>
       </Form.Group>
 
@@ -70,7 +93,11 @@ export default function Selling(props) {
         <Form.Label>Accrued Interest </Form.Label>
         <InputGroup>
           <InputGroup.Text>$</InputGroup.Text>
-          <Form.Control type="number" placeholder="" onChange={handleAccruedOnChange} />
+          <Form.Control
+            type="number"
+            placeholder=""
+            onChange={handleAccruedOnChange}
+          />
         </InputGroup>
       </Form.Group>
 
@@ -78,21 +105,27 @@ export default function Selling(props) {
         <Form.Label>Upgrading/Resale Levy</Form.Label>
         <InputGroup>
           <InputGroup.Text>$</InputGroup.Text>
-          <Form.Control type="number" placeholder="" onChange={handleLevyOnChange}/>
+          <Form.Control
+            type="number"
+            placeholder=""
+            onChange={handleLevyOnChange}
+          />
         </InputGroup>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="buyingHdbBool">
         <Form.Label>Are You Purchasing Another HDB? </Form.Label>
-        <Form.Check type="checkbox" />
+        <Form.Check type="checkbox" onChange={handleNextHdb} />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="usingHleBool">
         <Form.Label>Will you be taking HDB Loan for the next HDB?</Form.Label>
-        <Form.Check type="checkbox" />
+        <Form.Check disabled={disabledNextHleInput} type="checkbox" onChange={handleNextHle}/>
       </Form.Group>
 
-      <Button type="submit" variant="success">Submit</Button>
+      <Button type="submit" variant="success">
+        Submit
+      </Button>
     </Form>
   );
 }
