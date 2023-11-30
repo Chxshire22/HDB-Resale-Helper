3bf1ff6cd07ee25e3fa0f90df22df52d45aab639
Chxshire22 <akuindra22@gmail.com>
2023-12-01 01:20:58 +0800

    try to make form fields for buyfees and fixed btn

--- src/BuyingCosts.jsx
+++ src/BuyingCosts.jsx
@@ -20,21 +20,55 @@ export default function BuyingCosts(props) {
       </Link>
       <h1 className="container__block-header">Buying Fees</h1>
       <Form onSubmit={handleSubmit}>
+
+        
         <Form.Group className="mb-3 form-group">
-          <Form.Label>test</Form.Label>
+          <Form.Label>Purchase Price</Form.Label>
           <InputGroup>
+            <InputGroup.Text>$</InputGroup.Text>
+            <Form.Control type="number"></Form.Control>
+          </InputGroup>
+        </Form.Group>
+
+        <Form.Group className="mb-3 form-group">
+          <Form.Label>Agent Commission</Form.Label>
+          <InputGroup>
+            <InputGroup.Text>$</InputGroup.Text>
             <Form.Control></Form.Control>
           </InputGroup>
         </Form.Group>
+        <Form.Group className="mb-3 form-group">
+          <Form.Label>Valuation Fee</Form.Label>
+          <InputGroup>
+            <InputGroup.Text>$</InputGroup.Text>
+            <Form.Control></Form.Control>
+          </InputGroup>
+        </Form.Group>
+        <Form.Group className="mb-3 form-group">
+          <Form.Label>Resale Application Fee</Form.Label>
+          <InputGroup>
+            <InputGroup.Text>$</InputGroup.Text>
+            <Form.Control></Form.Control>
+          </InputGroup>
+        </Form.Group>
+        <Form.Group className="mb-3 form-group">
+          <Form.Label>Conveyancing Fee</Form.Label>
+          <InputGroup>
+            <InputGroup.Text>$</InputGroup.Text>
+            <Form.Control></Form.Control>
+          </InputGroup>
+        </Form.Group>
+
+
       </Form>
 
-      <p className="return-action">
+      <div className="return-action">
         <Link to="/buying">
           <Button className="btn btn-blue" variant="primary">
             Back
           </Button>
         </Link>{" "}
-      </p>
+      </div>
     </div>
   );
 }
